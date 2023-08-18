e462da260af0307ac4b0371471d13d66e2fd302a
Clemamolette <clementinegaga79@outlook.fr>
2023-08-18 14:41:23 +0200

    Added right skippable step

--- components/Step.vue
+++ components/Step.vue
@@ -38,10 +38,8 @@ const { current_step_index, steps } = toRefs(stepper_tree)
 
 function skippable () {
   if (stepper_tree.steps[step_index].component.skippable !== undefined) {
-    console.log("not undefined")
     return(stepper_tree.steps[step_index].component.skippable)
   } else {
-    console.log("undefined")
     return(false)
   }
 }
