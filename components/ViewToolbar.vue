5c808e96b435952a9cdcc7bc4362e39d5044ffec
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-09-27 16:31:23 +0200

    fix(toolbar): fix reset icon

--- components/ViewToolbar.vue
+++ components/ViewToolbar.vue
@@ -1,30 +1,34 @@
 <template>
-    <v-row dense :class="[$style.floatToolbar, 'flex-column']">
-        <v-col>
-            <v-tooltip location="left">
-                Reset camera
-                <template #activator="{ props }">
-                    <v-btn @click.stop=reset_camera icon="mdi-crop-free" density="comfortable" v-bind="props" />
-                </template>
-            </v-tooltip>
-        </v-col>
-    </v-row>
+  <v-row dense :class="[$style.floatToolbar, 'flex-column']">
+    <v-col>
+      <v-tooltip location="left">
+        Reset camera
+        <template #activator="{ props }">
+          <v-btn @click.stop=reset_camera density="comfortable" v-bind="props" icon>
+            <v-icon size="32">
+              mdi-cube-scan
+            </v-icon>
+          </v-btn>
+        </template>
+      </v-tooltip>
+    </v-col>
+  </v-row>
 </template>
 
 <script setup>
 const viewer_store = use_viewer_store()
-function reset_camera() {
-    viewer_store.reset_camera()
+function reset_camera () {
+  viewer_store.reset_camera()
 }
 </script>
 
 <style module>
 .floatToolbar {
-    position: absolute;
-    z-index: 2;
-    right: 20px;
-    top: 20px;
-    background-color: rgba(0, 0, 0, 0.4);
-    border-radius: 16px;
+  position: absolute;
+  z-index: 2;
+  right: 20px;
+  top: 20px;
+  background-color: rgba(0, 0, 0, 0.4);
+  border-radius: 16px;
 }
 </style>
