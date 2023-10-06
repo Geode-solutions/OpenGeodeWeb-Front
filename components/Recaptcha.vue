494ed45f5bc1844dfd7251410bbff5231a73e669
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-10-06 17:14:08 +0200

    pull from next

--- components/Recaptcha.vue
+++ components/Recaptcha.vue
@@ -10,32 +10,32 @@
 </template>
 
 <script setup>
-import { VueRecaptcha } from "vue-recaptcha";
+  import { VueRecaptcha } from "vue-recaptcha"
 
-const websocket_store = use_websocket_store();
-const cloud_store = use_cloud_store();
-const { is_captcha_validated } = storeToRefs(cloud_store);
+  const websocket_store = use_websocket_store()
+  const cloud_store = use_cloud_store()
+  const { is_captcha_validated } = storeToRefs(cloud_store)
 
-const site_key = useRuntimeConfig().public.RECAPTCHA_SITE_KEY;
+  const site_key = useRuntimeConfig().public.RECAPTCHA_SITE_KEY
 
-onMounted(() => {
-  if (process.client) {
-    const config = useRuntimeConfig();
-    if (config.public.NODE_ENV !== "production") {
-      cloud_store.$patch({ is_captcha_validated: true });
+  onMounted(() => {
+    if (process.client) {
+      const config = useRuntimeConfig()
+      if (config.public.NODE_ENV !== "production") {
+        cloud_store.$patch({ is_captcha_validated: true })
+      }
+    }
+  })
+
+  async function submit_recaptcha(token) {
+    try {
+      const response = await $fetch.raw(
+        `/.netlify/functions/recaptcha?token=${token}`,
+      )
+      cloud_store.$patch({ is_captcha_validated: response.status == 200 })
+      recaptcha.reset()
+    } catch (error) {
+      console.error(error)
     }
   }
-});
-
-async function submit_recaptcha(token) {
-  try {
-    const response = await $fetch.raw(
-      `/.netlify/functions/recaptcha?token=${token}`
-    );
-    cloud_store.$patch({ is_captcha_validated: response.status == 200 });
-    recaptcha.reset();
-  } catch (error) {
-    console.error(error);
-  }
-}
 </script>
