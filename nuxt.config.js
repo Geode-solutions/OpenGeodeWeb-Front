d3822d77640119d6f96ec63fec05f2854b3b3cfe
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-12-01 16:08:22 +0100

    add publicRuntimeConfig

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,16 @@
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      VIEWER_PROTOCOL: process.env.NODE_ENV === "production" ? "wss" : "ws",
+      GEODE_PROTOCOL: process.env.NODE_ENV === "production" ? "https" : "http",
+      VIEWER_PORT: process.env.NODE_ENV === "production" ? "443" : "1234",
+      GEODE_PORT: process.env.NODE_ENV === "production" ? "443" : "5000",
+      SITE_BRANCH:
+        process.env.NODE_ENV === "production" ? process.env.SITE_BRANCH : "",
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  },
+
   imports: {
     dirs: ["stores"],
   },
