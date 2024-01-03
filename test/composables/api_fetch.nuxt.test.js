2f9acf286eb9a39b7860375008f44595d8cab7f0
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-03 17:23:51 +0100

    test interceptors

--- test/composables/api_fetch.nuxt.test.js
+++ test/composables/api_fetch.nuxt.test.js
@@ -1,32 +1,74 @@
-import { describe, expect, test, afterEach } from "vitest"
+import { describe, expect, test, beforeEach } from "vitest"
 import { registerEndpoint } from "@nuxt/test-utils/runtime"
+
+const schema = {
+  $id: "/test",
+  type: "object",
+  method: "POST",
+  properties: {
+    test: {
+      type: "string",
+    },
+  },
+  required: ["test"],
+  additionalProperties: false,
+}
 
 describe("api_fetch.js", () => {
   const errors_store = use_errors_store()
-  afterEach(async () => {
+  beforeEach(async () => {
     await errors_store.$patch({ errors: [] })
   })
-  test("Wrong params", async () => {
-    const schema = {
-      $id: "/test/",
-      type: "object",
-      method: "POST",
-      properties: {
-        test: {
-          type: "string",
-        },
-      },
-      required: ["test"],
-      additionalProperties: false,
-    }
+
+  test("Ajv wrong params", async () => {
+    registerEndpoint(schema.$id, {
+      method: schema.method,
+      handler: () => ({ return: "toto" }),
+    })
     const params = {}
     await api_fetch({ schema, params })
     expect(errors_store.errors.length).toBe(1)
+    expect(errors_store.errors[0].code).toBe(400)
   })
 
-  test("Ajv validation", async () => {
+  test("onRequestError", async () => {
+    const params = { test: "test" }
+    var request_error_value
+    await api_fetch(
+      { schema, params },
+      {
+        request_error_function: () => {
+          request_error_value = "error"
+        },
+      },
+    )
+    expect(errors_store.errors.length).toBe(1)
+    expect(errors_store.errors[0].code).toBe(404)
+    expect(request_error_value).toBe("error")
+  })
+
+  test("onResponse", async () => {
+    registerEndpoint(schema.$id, {
+      method: schema.method,
+      handler: () => ({ return: "toto" }),
+    })
+    const params = { test: "test" }
+    var response_value
+    await api_fetch(
+      { schema, params },
+      {
+        response_function: (response) => {
+          response_value = response._data.return
+        },
+      },
+    )
+    expect(errors_store.errors.length).toBe(0)
+    expect(response_value).toBe("toto")
+  })
+
+  test("onResponseError", async () => {
     const schema = {
-      $id: "/test",
+      $id: "/toto",
       type: "object",
       method: "POST",
       properties: {
@@ -38,12 +80,16 @@ describe("api_fetch.js", () => {
       additionalProperties: false,
     }
     const params = { test: "test" }
-    registerEndpoint("/test", {
-      method: schema.method,
-      handler: () => ({ test: "toto" }),
-    })
-    const reponse = await api_fetch({ schema, params })
-    expect(errors_store.errors.length).toBe(0)
-    expect(reponse._data.test).toBe("toto")
+    var response_error_value
+    await api_fetch(
+      { schema, params },
+      {
+        response_error_function: () => {
+          response_error_value = "error"
+        },
+      },
+    )
+    expect(errors_store.errors.length).toBe(1)
+    expect(response_error_value).toBe("error")
   })
 })
