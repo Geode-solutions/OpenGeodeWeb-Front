0849e5c202d2a00ea8a25c657b92a1e86bfe4444
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-09 14:56:32 +0100

    remove vuetify theme

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,30 +1,9 @@
 import { createVuetify } from "vuetify"
 
 import "@mdi/font/css/materialdesignicons.css"
-import colors from "vuetify/lib/util/colors"
 import "vuetify/styles"
 
-const light_theme = {
-  dark: false,
-  colors: {
-    background: "#FFFFFF",
-    primary: colors.teal.darken1,
-    secondary: colors.teal.lighten4,
-    accent: colors.amber.accent4,
-    error: colors.red.lighten2,
-    info: colors.yellow.accent4,
-    success: colors.green.accent4,
-    warning: colors.orange.accent4,
-  },
-}
-
 export const vuetify = createVuetify({
-  theme: {
-    defaultTheme: "light_theme",
-    themes: {
-      light_theme,
-    },
-  },
   defaultAssets: false,
   icons: {
     defaultSet: "mdi",
