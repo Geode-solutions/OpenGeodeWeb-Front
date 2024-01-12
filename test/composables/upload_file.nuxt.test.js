be4348127949cca9a99863fcba3b89dea0b2da08
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-12 15:38:23 +0100

    test upload_file

--- test/composables/upload_file.nuxt.test.js
+++ test/composables/upload_file.nuxt.test.js
@@ -1,112 +1,18 @@
 import { describe, expect, test, beforeEach } from "vitest"
 import { registerEndpoint } from "@nuxt/test-utils/runtime"
 
-describe("api_fetch.js", () => {
+describe("upload_file.js", () => {
+  const route = "/upload_file"
   const errors_store = use_errors_store()
-
-  const schema = {
-    $id: "/test",
-    type: "object",
-    method: "POST",
-    properties: {
-      test: {
-        type: "string",
-      },
-    },
-    required: ["test"],
-    additionalProperties: false,
-  }
   beforeEach(async () => {
     await errors_store.$patch({ errors: [] })
   })
 
-  test("Ajv wrong params", async () => {
-    registerEndpoint(schema.$id, {
-      method: schema.method,
-      handler: () => ({ return: "toto" }),
-    })
-    const params = {}
-    try {
-      await api_fetch({ schema, params })
-    } catch (error) {
-      expect(error.message).toBe(
-        "/test: data must have required property 'test'",
-      )
-    }
-    expect(errors_store.errors.length).toBe(1)
-    expect(errors_store.errors[0].code).toBe(400)
-  })
-
-  // test("onRequestError", async () => {
-  //   const schema = {
-  //     $id: "/test",
-  //     type: "object",
-  //     method: "POST",
-  //     properties: {
-  //       test: {
-  //         type: "string",
-  //       },
-  //     },
-  //     required: ["test"],
-  //     additionalProperties: false,
-  //   }
-  //   registerEndpoint(schema.$id, {
-  //     method: schema.method,
-  //     handler: async () => {
-  //       setTimeout(() => {}, 100 * 1000)
-  //     },
-  //   })
-  //   const params = { test: "test" }
-  //   var request_error_value
-  //   await api_fetch(
-  //     { schema, params },
-  //     {
-  //       request_error_function: () => {
-  //         request_error_value = "error"
-  //       },
-  //     },
-  //   )
-  //   expect(errors_store.errors.length).toBe(1)
-  //   expect(errors_store.errors[0].code).toBe(404)
-  //   expect(request_error_value).toBe("error")
-  // })
-
-  test("onResponse", async () => {
-    registerEndpoint(schema.$id, {
-      method: schema.method,
-      handler: () => ({ return: "toto" }),
-    })
-    const params = { test: "test" }
-    var response_value
-    await api_fetch(
-      { schema, params },
-      {
-        response_function: (response) => {
-          response_value = response._data.return
-        },
-      },
-    )
-    expect(errors_store.errors.length).toBe(0)
-    expect(response_value).toBe("toto")
-  })
-
   test("onResponseError", async () => {
-    const schema = {
-      $id: "/toto",
-      type: "object",
-      method: "POST",
-      properties: {
-        test: {
-          type: "string",
-        },
-      },
-      required: ["test"],
-      additionalProperties: false,
-    }
-    const params = { test: "test" }
+    let file = new FileReader()
     var response_error_value
-    await api_fetch(
-      { schema, params },
+    await upload_file(
+      { route, file },
       {
         response_error_function: () => {
           response_error_value = "error"
@@ -115,5 +21,23 @@ describe("api_fetch.js", () => {
     )
     expect(errors_store.errors.length).toBe(1)
     expect(response_error_value).toBe("error")
+  })
+  test("onResponse", async () => {
+    registerEndpoint(route, {
+      method: "PUT",
+      handler: () => ({ test: "ok" }),
+    })
+    let file = new FileReader()
+    var response_value
+    await upload_file(
+      { route, file },
+      {
+        response_function: () => {
+          response_value = "ok"
+        },
+      },
+    )
+    expect(errors_store.errors.length).toBe(0)
+    expect(response_value).toBe("ok")
   })
 })
