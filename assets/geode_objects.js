d15c0dda8999729ef1e6b2554995f42ed60c96e5
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-12 10:48:02 +0100

    revert geode_objects.js

--- assets/geode_objects.js
+++ assets/geode_objects.js
@@ -1,16 +1,110 @@
-const fs = require("fs")
-const path = require("path")
+import BRep from "@/assets/img/geode_objects/BRep.svg"
+import CrossSection from "@/assets/img/geode_objects/CrossSection.svg"
+import EdgedCurve2D from "@/assets/img/geode_objects/EdgedCurve2D.svg"
+import EdgedCurve3D from "@/assets/img/geode_objects/EdgedCurve3D.svg"
+import Graph from "@/assets/img/geode_objects/Graph.svg"
+import HybridSolid3D from "@/assets/img/geode_objects/HybridSolid3D.svg"
+import PointSet2D from "@/assets/img/geode_objects/PointSet2D.svg"
+import PointSet3D from "@/assets/img/geode_objects/PointSet3D.svg"
+import PolygonalSurface2D from "@/assets/img/geode_objects/PolygonalSurface2D.svg"
+import PolygonalSurface3D from "@/assets/img/geode_objects/PolygonalSurface3D.svg"
+import PolyhedralSolid3D from "@/assets/img/geode_objects/PolyhedralSolid3D.svg"
+import RasterImage2D from "@/assets/img/geode_objects/RasterImage2D.svg"
+import RasterImage3D from "@/assets/img/geode_objects/RasterImage3D.svg"
+import RegularGrid2D from "@/assets/img/geode_objects/RegularGrid2D.svg"
+import RegularGrid3D from "@/assets/img/geode_objects/RegularGrid3D.svg"
+import Section from "@/assets/img/geode_objects/Section.svg"
+import StructuralModel from "@/assets/img/geode_objects/StructuralModel.svg"
+import TetrahedralSolid3D from "@/assets/img/geode_objects/TetrahedralSolid3D.svg"
+import TriangulatedSurface2D from "@/assets/img/geode_objects/TriangulatedSurface2D.svg"
+import TriangulatedSurface3D from "@/assets/img/geode_objects/TriangulatedSurface3D.svg"
+import VertexSet from "@/assets/img/geode_objects/VertexSet.svg"
 
-const basedir = "./assets/img/geode_objects"
-const files = fs.readdirSync(basedir)
-
-var geode_objects = {}
-for (const geode_object of files) {
-  const class_name = geode_object.replace(".svg", "")
-  geode_objects[class_name] = {
-    tooltip: class_name,
-    image: path.join("assets/img/geode_objects", geode_object),
-  }
+const geode_objects = {
+  BRep: {
+    tooltip: "BRep",
+    image: BRep,
+  },
+  CrossSection: {
+    tooltip: "CrossSection",
+    image: CrossSection,
+  },
+  EdgedCurve2D: {
+    tooltip: "EdgedCurve2D",
+    image: EdgedCurve2D,
+  },
+  EdgedCurve3D: {
+    tooltip: "EdgedCurve3D",
+    image: EdgedCurve3D,
+  },
+  Graph: {
+    tooltip: "Graph",
+    image: Graph,
+  },
+  HybridSolid3D: {
+    tooltip: "HybridSolid3D",
+    image: HybridSolid3D,
+  },
+  PointSet2D: {
+    tooltip: "PointSet2D",
+    image: PointSet2D,
+  },
+  PointSet3D: {
+    tooltip: "PointSet3D",
+    image: PointSet3D,
+  },
+  PolygonalSurface2D: {
+    tooltip: "PolygonalSurface2D",
+    image: PolygonalSurface2D,
+  },
+  PolygonalSurface3D: {
+    tooltip: "PolygonalSurface3D",
+    image: PolygonalSurface3D,
+  },
+  PolyhedralSolid3D: {
+    tooltip: "PolyhedralSolid3D",
+    image: PolyhedralSolid3D,
+  },
+  RasterImage2D: {
+    tooltip: "RasterImage2D",
+    image: RasterImage2D,
+  },
+  RasterImage3D: {
+    tooltip: "RasterImage3D",
+    image: RasterImage3D,
+  },
+  RegularGrid2D: {
+    tooltip: "RegularGrid2D",
+    image: RegularGrid2D,
+  },
+  RegularGrid3D: {
+    tooltip: "RegularGrid3D",
+    image: RegularGrid3D,
+  },
+  Section: {
+    tooltip: "Section",
+    image: Section,
+  },
+  StructuralModel: {
+    tooltip: "StructuralModel",
+    image: StructuralModel,
+  },
+  TetrahedralSolid3D: {
+    tooltip: "TetrahedralSolid3D",
+    image: TetrahedralSolid3D,
+  },
+  TriangulatedSurface2D: {
+    tooltip: "TriangulatedSurface2D",
+    image: TriangulatedSurface2D,
+  },
+  TriangulatedSurface3D: {
+    tooltip: "TriangulatedSurface3D",
+    image: TriangulatedSurface3D,
+  },
+  VertexSet: {
+    tooltip: "VertexSet",
+    image: VertexSet,
+  },
 }
 
 export default geode_objects
