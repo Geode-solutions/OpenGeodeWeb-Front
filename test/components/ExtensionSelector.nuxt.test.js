adac13d3aa7e00df38afada1997b8d247bb3f4cd
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-15 17:44:31 +0100

    feat(test): test ExtensionSelector

---
+++ test/components/ExtensionSelector.nuxt.test.js
@@ -0,0 +1,48 @@
+// @vitest-environment nuxt
+
+import { describe, expect, test } from "vitest"
+import { registerEndpoint, mountSuspended } from "@nuxt/test-utils/runtime"
+
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+import ExtensionSelector from "@/components/ExtensionSelector.vue"
+import schema from "@/assets/schemas/ExtensionSelector.json"
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+global.ResizeObserver = require("resize-observer-polyfill")
+
+describe("ExtensionSelector.vue", async () => {
+  test(`BRep`, async () => {
+    const geode_object = "BRep"
+    registerEndpoint(schema.$id, {
+      method: schema.method,
+      handler: () => ({
+        geode_objects_and_output_extensions: { BRep: { is_saveable: true } },
+      }),
+    })
+    const wrapper = await mountSuspended(ExtensionSelector, {
+      global: {
+        plugins: [vuetify],
+      },
+      props: { input_geode_object: "BRep", filenames: ["test.toto"] },
+    })
+    const first_v_card = wrapper.findComponent(components.VCard)
+    console.log("first_v_card", first_v_card)
+    const second_v_card = first_v_card.findComponent(components.VCard)
+    console.log("second_v_card", second_v_card)
+
+    await second_v_card.trigger("click")
+    expect(wrapper.emitted()).toHaveProperty("update_values")
+    expect(wrapper.emitted().update_values).toHaveLength(1)
+    expect(wrapper.emitted().update_values[0][0]).toEqual({
+      output_geode_object: geode_object,
+    })
+  })
+})
+// }
