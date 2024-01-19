a09e243a505b55ad5a62e6b310c727a129b79e52
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-01-19 09:09:35 +0100

    test runFunctionIfCloudRunning

--- test/composables/runFunctionIfCloudRunning.nuxt.test.js
+++ test/composables/runFunctionIfCloudRunning.nuxt.test.js
@@ -1,26 +1,39 @@
+import { setActivePinia, createPinia } from "pinia"
 import { describe, expect, test, beforeEach, vi } from "vitest"
 
 describe("api_fetch.js", () => {
   const geode_store = use_geode_store()
-  const viewer_store = use_viewer_store()
+  const websocket_store = use_websocket_store()
+  const cloud_store = use_cloud_store()
+
+  const dumb_obj = { dumb_method: () => true }
 
   beforeEach(async () => {
+    setActivePinia(createPinia())
     await geode_store.$patch({ is_running: false })
-    await viewer_store.$patch({ is_running: false })
+    await websocket_store.$patch({ is_running: false })
   })
-  function dumb() {
-    console.log("TOTO")
-    return 1
-  }
+
   test("is_running true", async () => {
-    const spy = vi.spyOn(dumb)
-
+    const spy = vi.spyOn(dumb_obj, "dumb_method")
+    console.log("cloud_store.is_running", cloud_store.is_running)
     await geode_store.$patch({ is_running: true })
+    await websocket_store.$patch({ is_running: true })
 
-    // dumb()
-    expect(spy).toHaveBeenCalled()
-    console.log(geode_store.is_running)
-    runFunctionIfCloudRunning(dumb())
+    await runFunctionIfCloudRunning(dumb_obj.dumb_method)
     expect(spy).toHaveBeenCalled()
   })
+
+  //   test("is_running false", async () => {
+  //     const spy = vi.spyOn(dumb_obj, "dumb_method")
+  //     runFunctionIfCloudRunning(dumb_obj.dumb_method)
+  //     // expect(spy).not.toHaveBeenCalled()
+  //     await geode_store.$patch({ is_running: true })
+  //     await websocket_store.$patch({ is_running: true })
+  //     console.log("geode_store.is_running", geode_store.is_running)
+  //     console.log("websocket_store.is_running", websocket_store.is_running)
+  //     console.log("cloud_store.is_running", cloud_store.is_running)
+
+  //     expect(spy).toHaveBeenCalled()
+  //   })
 })
