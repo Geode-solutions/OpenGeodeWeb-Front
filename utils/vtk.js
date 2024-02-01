ff6beefc8d2f271aab20c5730a88c44b9b8dbc4a
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-02-01 15:44:53 +0100

    feat(utils): generate vtk_protocols

--- utils/vtk.js
+++ utils/vtk.js
@@ -1,26 +1,29 @@
 /* eslint-disable arrow-body-style */
 function createMethods(session) {
-  return {
-    create_object_pipeline: (params) =>
-      session.call("create_object_pipeline", [params]),
-    create_visualization: () => session.call("create_visualization", []),
-    reset: () => session.call("reset"),
-    reset_camera: () => session.call("reset_camera", []),
-    toggle_object_visibility: (params) =>
-      session.call("toggle_object_visibility", [params]),
-    toggle_edge_visibility: (params) =>
-      session.call("toggle_edge_visibility", [params]),
-    toggle_point_visibility: (params) =>
-      session.call("toggle_point_visibility", [params]),
-    set_color: (params) => session.call("set_color", [params]),
-    set_vertex_attribute: (params) =>
-      session.call("set_vertex_attribute", [params]),
-    point_size: (params) => session.call("point_size", [params]),
-    apply_textures: (params) => session.call("apply_textures", [params]),
-    get_point_position: (params) =>
-      session.call("get_point_position", [params]),
-    update_data: (params) => session.call("update_data", [params]),
+  const functions_array = [
+    "apply_textures",
+    "create_object_pipeline",
+    "create_visualization",
+    "get_point_position",
+    "point_size",
+    "reset",
+    "reset_camera",
+    "set_color",
+    "set_vertex_attribute",
+    "toggle_edge_visibility",
+    "toggle_object_visibility",
+    "toggle_point_visibility",
+    "update_data",
+  ]
+
+  functions_array.sort()
+
+  var functions_object = {}
+  for (const function_name of functions_array) {
+    functions_object[function_name] = (params) =>
+      session.call(function_name, [params])
   }
+  return functions_object
 }
 
 export default createMethods
