a19a1ed06c6de75a2faed1176d1b5df31254e45b
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-02-27 17:12:53 +0100

    add rpc keyword

--- utils/validate_schema.js
+++ utils/validate_schema.js
@@ -2,7 +2,7 @@ import Ajv from "ajv"
 
 export function validate_schema(schema, body) {
   const ajv = new Ajv()
-  const list_keywords = ["methods", "route", "max_retry"]
+  const list_keywords = ["methods", "route", "max_retry", "rpc"]
   for (const keyword of list_keywords) {
     ajv.addKeyword(keyword)
   }
