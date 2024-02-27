8ee6160bbcab75221083aa04e932c047b39b96ea
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-02-27 09:49:56 +0100

    return promise

--- composables/viewer_call.js
+++ composables/viewer_call.js
@@ -28,42 +28,48 @@ export function viewer_call(
     params = []
   }
 
-  if (client) {
-    viewer_store.start_request()
-    client
-      .getConnection()
-      .getSession()
-      .call(schema.route, params)
-      .then(
-        (value) => {
-          if (response_function) {
-            console.log("response_function", value)
-            response_function(value)
+  let promise = new Promise((resolve, reject) => {
+    if (client) {
+      viewer_store.start_request()
+      client
+        .getConnection()
+        .getSession()
+        .call(schema.route, params)
+        .then(
+          (value) => {
+            if (response_function) {
+              console.log("response_function", value)
+              response_function(value)
+            }
+            resolve()
+          },
+          (reason) => {
+            if (request_error_function) {
+              console.log("request_error_function", reason)
+              request_error_function(reason)
+            }
+            reject()
+          },
+        )
+        .catch((error) => {
+          console.log("error : ", error)
+          errors_store.add_error({
+            code: error.code,
+            route: schema.route,
+            name: error.data.message,
+            description: error.data.exception,
+          })
+          if (response_error_function) {
+            response_error_function(error)
           }
-        },
-        (reason) => {
-          if (request_error_function) {
-            console.log("request_error_function", reason)
-            request_error_function(reason)
-          }
-        },
-      )
-      .catch((error) => {
-        console.log("error : ", error)
-        // errors_store.add_error({
-        //   code: error.code,
-        //   route: schema.route,
-        //   name: error.data.message,
-        //   description: error.data.exception,
-        // })
-        if (response_error_function) {
-          response_error_function(error)
-        }
-      })
-      .finally(() => {
-        viewer_store.stop_request()
-      })
-  }
+          reject()
+        })
+        .finally(() => {
+          viewer_store.stop_request()
+        })
+    }
+  })
+  return promise
 }
 
 export default viewer_call
