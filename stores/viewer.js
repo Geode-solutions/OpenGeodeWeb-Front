d005e71f3c807d6145fb3725cd0465074b2dc967
SpliiT <spliitdev@gmail.com>
2024-06-20 15:47:30 +0200

    added promise

--- stores/viewer.js
+++ stores/viewer.js
@@ -1,12 +1,11 @@
 import _ from "lodash"
 import vtkWSLinkClient from "@kitware/vtk.js/IO/Core/WSLinkClient"
 import "@kitware/vtk.js/Rendering/OpenGL/Profiles/Geometry"
+import { connectImageStream } from "@kitware/vtk.js/Rendering/Misc/RemoteView"
 import schemas from "@geode/opengeodeweb-viewer/schemas.json"
 
 export const use_viewer_store = defineStore("viewer", {
   state: () => ({
-    PROTOCOL: use_infra_store().is_cloud ? "wss" : "ws",
-    PORT: use_infra_store().is_cloud ? "443" : "1234",
     client: {},
     config: null,
     is_running: false,
@@ -15,16 +14,17 @@ export const use_viewer_store = defineStore("viewer", {
     request_counter: 0,
   }),
   getters: {
-    base_url(state) {
-      const infra_store = use_infra_store()
-      var viewer_url = `${state.PROTOCOL}://${infra_store.domain_name}:${state.PORT}`
-      if (infra_store.is_cloud) {
-        viewer_url += `/${infra_store.ID}/viewer`
+    base_url: () => {
+      const cloud_store = use_cloud_store()
+      const public_runtime_config = useRuntimeConfig().public
+      var viewer_url = `${public_runtime_config.VIEWER_PROTOCOL}://${public_runtime_config.API_URL}:${public_runtime_config.VIEWER_PORT}`
+      if (process.env.NODE_ENV == "production") {
+        viewer_url += `/${cloud_store.ID}/viewer`
       }
       viewer_url += "/ws"
       return viewer_url
     },
-    is_busy(state) {
+    is_busy: (state) => {
       return state.request_counter > 0
     },
   },
@@ -58,6 +58,7 @@ export const use_viewer_store = defineStore("viewer", {
       if (_.isEmpty(clientToConnect)) {
         clientToConnect = vtkWSLinkClient.newInstance()
       }
+
       // Connect to busy store
       clientToConnect.onBusyChange((count) => {
         this.buzy = count
@@ -84,25 +85,28 @@ export const use_viewer_store = defineStore("viewer", {
       const { connectImageStream } = await import(
         "@kitware/vtk.js/Rendering/Misc/RemoteView"
       )
-      clientToConnect
-        .connect(config)
-        .then((validClient) => {
-          connectImageStream(validClient.getConnection().getSession())
-          this.client = validClient
-          clientToConnect.endBusy()
+      return new Promise((resolve, reject) => {
+        clientToConnect
+          .connect(config)
+          .then((validClient) => {
+            connectImageStream(validClient.getConnection().getSession())
+            this.client = validClient
+            clientToConnect.endBusy()
 
-          // Now that the client is ready let's setup the server for us
-          viewer_call({
-            schema: schemas.opengeodeweb_viewer.create_visualization,
+            // Now that the client is ready let's setup the server for us
+            viewer_call({
+              schema: schemas.opengeodeweb_viewer.create_visualization,
+            })
+            viewer_call({
+              schema: schemas.opengeodeweb_viewer.reset,
+            })
+            this.is_running = true
+            resolve()
           })
-          viewer_call({
-            schema: schemas.opengeodeweb_viewer.reset,
+          .catch((error) => {
+            reject(error)
           })
-          this.is_running = true
-        })
-        .catch((error) => {
-          console.error(error)
-        })
+      })
     },
     start_request() {
       this.request_counter++
