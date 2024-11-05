a257db2e2edc056bc5c88d66ae9f3052fe1539b1
JulienChampagnol <julien.champagnol@geode-solutions.com>
2024-11-05 14:10:12 +0100

    fix(ObjectSelector): bigger cards on small screens & thin panels

--- components/ObjectSelector.vue
+++ components/ObjectSelector.vue
@@ -1,7 +1,7 @@
 <template>
   <FetchingData v-if="loading" />
   <v-row v-else-if="Object.keys(allowed_objects).length" class="justify-left">
-    <v-col v-for="(value, key) in allowed_objects" :key="key" cols="2" md="2">
+    <v-col v-for="(value, key) in allowed_objects" :key="key" cols="2" md="4">
       <v-tooltip
         :text="
           value['is_loadable']
