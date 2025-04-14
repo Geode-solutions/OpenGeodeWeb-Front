8767e5aacccf1b6242255da1ea0aca70524e7509
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-04-14 10:23:56 +0200

    feat(feedback timeout): auto customisable timeout

--- stores/feedback.js
+++ stores/feedback.js
@@ -2,6 +2,7 @@ export const use_feedback_store = defineStore("feedback", {
   state: () => ({
     feedbacks: [],
     server_error: false,
+    feedbacks_timeout_miliseconds: 5000,
   }),
   actions: {
     async add_error(code, route, name, description) {
@@ -12,12 +13,20 @@ export const use_feedback_store = defineStore("feedback", {
         name,
         description,
       })
+      const feedback_index = this.feedbacks.length - 1
+      setTimeout(() => {
+        this.delete_feedback(feedback_index)
+      }, this.feedbacks_timeout_miliseconds)
     },
     async add_success(description) {
       await this.feedbacks.push({
         type: "success",
         description,
       })
+      const feedback_index = this.feedbacks.length - 1
+      setTimeout(() => {
+        this.delete_feedback(feedback_index)
+      }, this.feedbacks_timeout_miliseconds)
     },
     async delete_feedback(feedback_index) {
       await this.feedbacks.splice(feedback_index, 1)
