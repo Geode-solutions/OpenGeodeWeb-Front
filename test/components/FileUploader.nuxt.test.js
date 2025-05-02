f93b44e45208f3442b70ec28092e07a90bf2aad6
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-05-02 16:15:44 +0200

    test

--- test/components/FileUploader.nuxt.test.js
+++ test/components/FileUploader.nuxt.test.js
@@ -56,6 +56,7 @@ describe("FileUploader.vue", async () => {
 
       await v_btn.trigger("click")
       await flushPromises()
+      console.log("TEST WRAPPER EMITTED", wrapper.emitted())
       expect(wrapper.emitted().files_uploaded[0][0]).toEqual(files)
     })
 
