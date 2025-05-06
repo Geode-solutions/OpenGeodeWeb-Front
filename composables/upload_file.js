1400dffb325760643cc5949e954589e5c335e159
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-05-06 17:06:45 +0200

    unused log

--- composables/upload_file.js
+++ composables/upload_file.js
@@ -23,11 +23,7 @@ export async function upload_file(
   return $fetch(route, {
     baseURL: geode_store.base_url,
     ...request_options,
-    onRequest({ request }) {
-      console.log("onRequest", request)
-    },
     onRequestError({ error }) {
-      console.log("onRequestError", error)
       geode_store.stop_request()
       feedback_store.add_error(error.code, route, error.message, error.stack)
       if (request_error_function) {
@@ -35,7 +31,6 @@ export async function upload_file(
       }
     },
     onResponse({ response }) {
-      console.log("onResponse", response)
       if (response.ok) {
         geode_store.stop_request()
         if (response_function) {
@@ -44,7 +39,6 @@ export async function upload_file(
       }
     },
     onResponseError({ response }) {
-      console.log("onResponseError", response)
       geode_store.stop_request()
       feedback_store.add_error(
         response.status,
