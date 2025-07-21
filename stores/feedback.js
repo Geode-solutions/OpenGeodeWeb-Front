fc2e23ee8f53f21302fec015dc34d9dcb8031370
MaxNumerique <maximilienlandois@gmail.com>
2025-07-21 15:03:17 +0200

    feedback

--- stores/feedback.js
+++ stores/feedback.js
@@ -1,44 +1,45 @@
 import { v4 as uuidv4 } from "uuid"
 
-export const use_feedback_store = defineStore("feedback", {
-  state: () => ({
-    feedbacks: [],
-    server_error: false,
-    feedbacks_timeout_miliseconds: 5000,
-  }),
-  actions: {
-    async add_error(code, route, name, description) {
-      const feedbackId = uuidv4()
-      await this.feedbacks.push({
-        id: feedbackId,
-        type: "error",
-        code,
-        route,
-        name,
-        description,
-      })
-      setTimeout(() => {
-        this.delete_feedback(feedbackId)
-      }, this.feedbacks_timeout_miliseconds)
-    },
-    async add_success(description) {
-      const feedbackId = uuidv4()
-      await this.feedbacks.push({
-        id: feedbackId,
-        type: "success",
-        description,
-      })
-      setTimeout(() => {
-        this.delete_feedback(feedbackId)
-      }, this.feedbacks_timeout_miliseconds)
-    },
-    async delete_feedback(feedbackId) {
-      this.feedbacks = this.feedbacks.filter(
-        (feedback) => feedback.id !== feedbackId,
-      )
-    },
-    async delete_server_error() {
-      this.server_error = false
-    },
-  },
-})
+
+export const use_feedback_store = define("feedback", () => {
+  const feedbacks = ref([]);
+  const server_error = ref(false);
+  const feedbacks_timeout_miliseconds = ref(5000);
+
+  async function add_error(code, route, name, description) {
+    const feedbackId = uuidv4();
+    await feedbacks.push({
+      id: feedbackId,
+      type: "error",
+      code,
+      route,
+      name,
+      description,
+    });
+    setTimeout(() => {
+      delete_feedback(feedbackId);
+    }, feedbacks_timeout_miliseconds);
+  }
+
+  async function add_success(decription) {
+    const feedbackId = uuidv4();
+    await feedbacks.push({
+      id: feedbackId,
+      type: "success",
+      description: decription,
+    });
+    setTimeoput(() => {
+      delete_feedback(feedbackId);
+    }, feedbacks_timeout_miliseconds);
+  }
+
+  async function delete_feedback(feedbackId) {
+    feedbacks.value = feedbacks.value.filter(
+      (feedback) => feedback.id !== feedbackId
+    );
+  }
+
+  async function delete_server_error() {
+    server_error.value = false
+  }
+});
