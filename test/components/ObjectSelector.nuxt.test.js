d2ee03aa50f2865b43a5e32af4e74a56491a813c
MaxNumerique <maximilienlandois@gmail.com>
2025-08-05 22:40:58 +0200

    indent corrected

--- test/components/ObjectSelector.nuxt.test.js
+++ test/components/ObjectSelector.nuxt.test.js
@@ -82,50 +82,50 @@ describe("ObjectSelector.vue", async () => {
     expect(wrapper.emitted().update_values[0][0]).toEqual({
       input_geode_object: geode_object_1,
     })
+  })
 
-    test(`test object_priority with equal is_loadable scores`, async () => {
-      var response = {
-        allowed_objects: {},
-      }
-      const geode_object_1 = "BRep"
-      const geode_object_2 = "EdgedCurve3D"
-      const geode_object_3 = "PolygonalSurface3D"
-      response["allowed_objects"][geode_object_1] = { is_loadable: true }
-      response["allowed_objects"][geode_object_2] = { is_loadable: true }
-      response["allowed_objects"][geode_object_3] = { is_loadable: true }
-      registerEndpoint(allowed_objects.$id, {
-        method: allowed_objects.methods[0],
-        handler: () => response,
-      })
-      const object_priority_schema = schemas.opengeodeweb_back.object_priority
-      registerEndpoint(object_priority_schema.$id, {
-        method: object_priority_schema.methods[0],
-        handler: ({ body }) => {
-          const { object_name } = body
-          const priorities = {
-            "BRep": 10,
-            "EdgedCurve3D": 5,
-            "PolygonalSurface3D": 8
-          }
-          return { priority: priorities[object_name] || 0 }
+  test(`test object_priority with equal is_loadable scores`, async () => {
+    var response = {
+      allowed_objects: {},
+    }
+    const geode_object_1 = "BRep"
+    const geode_object_2 = "EdgedCurve3D"
+    const geode_object_3 = "PolygonalSurface3D"
+    response["allowed_objects"][geode_object_1] = { is_loadable: true }
+    response["allowed_objects"][geode_object_2] = { is_loadable: true }
+    response["allowed_objects"][geode_object_3] = { is_loadable: true }
+    registerEndpoint(allowed_objects.$id, {
+      method: allowed_objects.methods[0],
+      handler: () => response,
+    })
+    const object_priority_schema = schemas.opengeodeweb_back.object_priority
+    registerEndpoint(object_priority_schema.$id, {
+      method: object_priority_schema.methods[0],
+      handler: ({ body }) => {
+        const { object_name } = body
+        const priorities = {
+          "BRep": 10,
+          "EdgedCurve3D": 5,
+          "PolygonalSurface3D": 8
         }
-      })
-      const wrapper = await mountSuspended(ObjectSelector, {
-        global: {
-          plugins: [vuetify, pinia],
-        },
-        props: { filenames: ["test.toto"], supported_feature: "test" },
-      })
-      await flushPromises()
-      const v_cards = wrapper.findAllComponents(components.VCard)
-      expect(v_cards).toHaveLength(1)
-      const v_img = v_cards[0].findComponent(components.VImg)
-      expect(v_img.vm.src).toContain(`${geode_object_1}.svg`)
-      expect(wrapper.emitted()).toHaveProperty("update_values")
-      expect(wrapper.emitted().update_values).toHaveLength(1)
-      expect(wrapper.emitted().update_values[0][0]).toEqual({
-        input_geode_object: geode_object_1,
-      })
+        return { priority: priorities[object_name] || 0 }
+      }
+    })
+    const wrapper = await mountSuspended(ObjectSelector, {
+      global: {
+        plugins: [vuetify, pinia],
+      },
+      props: { filenames: ["test.toto"], supported_feature: "test" },
+    })
+    await flushPromises()
+    const v_cards = wrapper.findAllComponents(components.VCard)
+    expect(v_cards).toHaveLength(1)
+    const v_img = v_cards[0].findComponent(components.VImg)
+    expect(v_img.vm.src).toContain(`${geode_object_1}.svg`)
+    expect(wrapper.emitted()).toHaveProperty("update_values")
+    expect(wrapper.emitted().update_values).toHaveLength(1)
+    expect(wrapper.emitted().update_values[0][0]).toEqual({
+      input_geode_object: geode_object_1,
     })
   })
 })
