93b9e10a78ef7c1d19b999db7bdfba70d2f0271f
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-09-03 16:35:33 +0200

    spyOn viewer_call

--- tests/integration/stores/DataStyle/mesh/Edges.nuxt.test.js
+++ tests/integration/stores/DataStyle/mesh/Edges.nuxt.test.js
@@ -28,11 +28,12 @@ import { useDataBaseStore } from "@ogw_f/stores/data_base"
 import { useViewerStore } from "@ogw_f/stores/viewer"
 import { useInfraStore } from "@ogw_f/stores/infra"
 
-import { ResetPlugin } from "@ogw_f/plugins/reset_plugin.client"
-
+import { ResetPlugin } from "@ogw_f/plugins/reset_plugin"
 import viewer_call from "@ogw_f/composables/viewer_call"
-
+import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json"
 import { WebSocket } from "ws"
+
+const mesh_edges_schemas = viewer_schemas.opengeodeweb_viewer.mesh.edges
 
 const mockLockRequest = vi.fn().mockImplementation(async (name, callback) => {
   return callback({ name })
@@ -70,7 +71,6 @@ describe("Mesh edges", () => {
   const viewerStore = useViewerStore()
   const infraStore = useInfraStore()
 
-  console.log("dataStyleStore", dataStyleStore.$reset)
   beforeEach(async () => {
     await dataStyleStore.$reset()
     await dataBaseStore.$reset()
@@ -127,10 +127,16 @@ describe("Mesh edges", () => {
       const color = { r: 255, g: 0, b: 0 }
       const spy = vi.spyOn(composables, "viewer_call")
       await dataStyleStore.setEdgesColor(id, color)
-      // expect(spy).toHaveBeenCalledWith({
-      //   schema: mesh_edges_schemas.color,
-      //   params: { id, color },
-      // })
+      expect(spy).toHaveBeenCalledWith(
+        {
+          schema: mesh_edges_schemas.color,
+          params: { id, color },
+        },
+        {
+          response_function: expect.any(Function),
+        },
+      )
+      expect(spy).toHaveBeenCalledTimes(1)
       expect(dataStyleStore.edgesColor(id)).toStrictEqual(color)
       expect(viewerStore.status).toBe(status.CONNECTED)
     })
