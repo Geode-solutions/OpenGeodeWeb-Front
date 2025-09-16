09ebb7d76bdacea624be9db06f00de6d6e19a848
SpliiT <spliitdev@gmail.com>
2025-09-16 17:02:00 +0200

    fix(ContextMenuItem): Changing import

--- components/Viewer/PointSet/SpecificPointsOptions.vue
+++ components/Viewer/PointSet/SpecificPointsOptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <ContextMenuItem
+  <ViewerContextMenuItem
     :itemProps="props.itemProps"
     tooltip="Points options"
     :btn_image="props.btn_image"
@@ -36,7 +36,7 @@
         </v-row>
       </template>
     </template>
-  </ContextMenuItem>
+  </ViewerContextMenuItem>
 </template>
 
 <script setup>
