dea78ddfff4d3bdf1878e211e30f6ff547f84176
JulienChampagnol <julien.champagnol@geode-solutions.com>
2025-10-23 12:13:04 +0200

    polyhedra okay

--- tests/integration/stores/data_style/mesh/polyhedra.nuxt.test.js
+++ tests/integration/stores/data_style/mesh/polyhedra.nuxt.test.js
@@ -13,10 +13,10 @@ import { kill_back, kill_viewer } from "~/utils/local"
 import { setupIntegrationTests } from "../../../setup.js"
 
 // Local constants
-const mesh_polygons_schemas = viewer_schemas.opengeodeweb_viewer.mesh.polygons
+const mesh_polyhedra_schemas = viewer_schemas.opengeodeweb_viewer.mesh.polyhedra
 let id, back_port, viewer_port
-const file_name = "test.og_psf3d"
-const geode_object = "PolygonalSurface3D"
+const file_name = "test.og_rgd3d"
+const geode_object = "RegularGrid3D"
 const object_type = "mesh"
 
 beforeEach(async () => {
@@ -25,54 +25,54 @@ beforeEach(async () => {
     geode_object,
     object_type,
   ))
-  console.log("beforeEach polygons id", id)
+  console.log("beforeEach polyhedra id", id)
 }, 25000)
 
 afterEach(async () => {
   await Promise.all([kill_back(back_port), kill_viewer(viewer_port)])
 })
 
-describe("Mesh polygons", () => {
-  describe("Polygons visibility", () => {
+describe("Mesh polyhedra", () => {
+  describe("Polyhedra visibility", () => {
     test("Visibility true", async () => {
       const dataStyleStore = useDataStyleStore()
       const viewerStore = useViewerStore()
-      await dataStyleStore.setMeshPolygonsVisibility(id, true)
-      expect(dataStyleStore.meshPolygonsVisibility(id)).toBe(true)
+      await dataStyleStore.setMeshPolyhedraVisibility(id, true)
+      expect(dataStyleStore.meshPolyhedraVisibility(id)).toBe(true)
       expect(viewerStore.status).toBe(Status.CONNECTED)
     })
   })
-  describe("Polygons active coloring", () => {
+  describe("Polyhedra active coloring", () => {
     test("test coloring", async () => {
       const dataStyleStore = useDataStyleStore()
       const viewerStore = useViewerStore()
       const coloringTypes = ["color"]
       for (let i = 0; i < coloringTypes.length; i++) {
-        dataStyleStore.setMeshPolygonsActiveColoring(id, coloringTypes[i])
-        expect(dataStyleStore.meshPolygonsActiveColoring(id)).toBe(
+        dataStyleStore.setMeshPolyhedraActiveColoring(id, coloringTypes[i])
+        expect(dataStyleStore.meshPolyhedraActiveColoring(id)).toBe(
           coloringTypes[i],
         )
         expect(viewerStore.status).toBe(Status.CONNECTED)
       }
     })
   })
-  describe("Polygons color", () => {
+  describe("Polyhedra color", () => {
     test("test red", async () => {
       const dataStyleStore = useDataStyleStore()
       const viewerStore = useViewerStore()
       const color = { r: 255, g: 0, b: 0 }
       const spy = vi.spyOn(composables, "viewer_call")
-      await dataStyleStore.setMeshPolygonsColor(id, color)
+      await dataStyleStore.setMeshPolyhedraColor(id, color)
       expect(spy).toHaveBeenCalledWith(
         {
-          schema: mesh_polygons_schemas.color,
+          schema: mesh_polyhedra_schemas.color,
           params: { id, color },
         },
         {
           response_function: expect.any(Function),
         },
       )
-      expect(dataStyleStore.meshPolygonsColor(id)).toStrictEqual(color)
+      expect(dataStyleStore.meshPolyhedraColor(id)).toStrictEqual(color)
       expect(viewerStore.status).toBe(Status.CONNECTED)
     })
   })
