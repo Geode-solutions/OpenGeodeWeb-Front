24eb5b6facd1e7974c848cc82e33f721ec145068
MaxNumerique <maximilienlandois@gmail.com>
2025-11-03 17:11:19 +0100

    test

--- stores/data_style.js
+++ stores/data_style.js
@@ -1,6 +1,8 @@
 import useDataStyleState from "../internal_stores/data_style_state.js"
 import useMeshStyle from "../internal_stores/mesh/index.js"
 import useModelStyle from "../internal_stores/model/index.js"
+import { defineStore } from "pinia"
+import { useDataBaseStore } from "./data_base.js"
 
 export const useDataStyleStore = defineStore("dataStyle", () => {
   /** States **/
@@ -10,7 +12,9 @@ export const useDataStyleStore = defineStore("dataStyle", () => {
   const dataBaseStore = useDataBaseStore()
 
   /** Actions **/
-  // function applyDataStyle() {
+  function addDataStyle(id, geode_object, object_type) {
+    dataStyleState.styles[id] = getDefaultStyle(geode_object)
+    const promise_array = []
     if (object_type === "mesh") {
       promise_array.push(meshStyleStore.applyMeshDefaultStyle(id))
     } else if (object_type === "model") {
@@ -19,25 +23,16 @@ export const useDataStyleStore = defineStore("dataStyle", () => {
     } else {
       throw new Error("Unknown object type")
     }
-    await Promise.all(promise_array);
-    await rpc_call("opengeodeweb_viewer.viewer.render_now", {});
-    return true;
+    return Promise.all(promise_array)
   }
 
-  async function setVisibility(payloadOrId, visibility) {
+  // useDataStyleStore: setVisibility
+  function setVisibility(payloadOrId, visibility) {
     const id =
       typeof payloadOrId === "string"
         ? payloadOrId
         : payloadOrId?.id ?? payloadOrId?.data_id ?? payloadOrId?.model_id
     if (!id) return Promise.resolve([])
-
-    const meta = dataBaseStore.itemMetaDatas(id)
-    const object_type = meta?.object_type
-    if (!object_type) return Promise.resolve([])
-
-    if (!dataStyleState.styles[id]) {
-      await addDataStyle(id, meta.geode_object, object_type)
-    }
 
     const visible =
       typeof visibility === "boolean"
@@ -46,11 +41,16 @@ export const useDataStyleStore = defineStore("dataStyle", () => {
         ? !!payloadOrId.visible
         : true
 
+    const meta = dataBaseStore.itemMetaDatas(id)
+    const object_type = meta?.object_type
+    if (!object_type) return Promise.resolve([])
+
     if (object_type === "mesh") {
       return Promise.all([meshStyleStore.setMeshVisibility(id, visible)])
-    } else {
+    } else if (object_type === "model") {
       return Promise.all([modelStyleStore.setModelVisibility(id, visible)])
     }
+    return Promise.resolve([])
   }
 
   function setModelEdgesVisibility(id, visibility) {
