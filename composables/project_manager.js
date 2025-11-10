6a6187443fd4663629d3d838e79dca875f839950
MaxNumerique <maximilienlandois@gmail.com>
2025-11-10 10:50:08 +0100

    project_manager

--- composables/project_manager.js
+++ composables/project_manager.js
@@ -1,100 +1,149 @@
 import back_schemas from "@geode/opengeodeweb-back/opengeodeweb_back_schemas.json"
-import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json"
 import fileDownload from "js-file-download"
+import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json"
 
 export function useProjectManager() {
   const geode = useGeodeStore()
   const appStore = useAppStore()
 
-  async function exportProject() {
+  const exportProject = function () {
     geode.start_request()
-    try {
-      await useInfraStore().create_connection()
-      const snapshot = appStore.exportStores()
-      const schema = back_schemas.opengeodeweb_back.export_project
-      const defaultName = "project.zip"
+    const infraStore = useInfraStore()
+    const snapshot = appStore.exportStores()
+    const schema = back_schemas.opengeodeweb_back.export_project
+    const defaultName = "project.zip"
 
-      await api_fetch(
-        { schema, params: { snapshot, filename: defaultName } },
-        {
-          response_function: async (response) => {
-            const data = response._data
-            const downloadName =
-              response.headers?.get?.("new-file-name") || defaultName
-            fileDownload(data, downloadName)
+    return infraStore
+      .create_connection()
+      .then(function () {
+        return api_fetch(
+          { schema, params: { snapshot, filename: defaultName } },
+          {
+            response_function: function (response) {
+              const data = response._data
+              let downloadName = defaultName
+              if (
+                response &&
+                response.headers &&
+                typeof response.headers.get === "function"
+              ) {
+                const name = response.headers.get("new-file-name")
+                if (name) {
+                  downloadName = name
+                }
+              }
+              fileDownload(data, downloadName)
+            },
           },
-        },
-      )
-    } finally {
-      geode.stop_request()
-    }
+        )
+      })
+      .finally(function () {
+        geode.stop_request()
+      })
   }
 
-  async function importProjectFile(file) {
+  const importProjectFile = function (file) {
     geode.start_request()
-    try {
-      await useInfraStore().create_connection()
 
-      const viewerStore = useViewerStore()
-      await viewerStore.ws_connect()
+    const viewerStore = useViewerStore()
+    const dataBaseStore = useDataBaseStore()
+    const treeviewStore = useTreeviewStore()
+    const hybridViewerStore = useHybridViewerStore()
+    const infraStore = useInfraStore()
 
-      await viewer_call({
-        schema: viewer_schemas.opengeodeweb_viewer.import_project,
-        params: {},
+    return infraStore
+      .create_connection()
+      .then(function () {
+        return viewerStore.ws_connect()
       })
-      await viewer_call({
-        schema: viewer_schemas.opengeodeweb_viewer.viewer.reset_visualization,
-        params: {},
+      .then(function () {
+        return viewer_call({
+          schema: viewer_schemas.opengeodeweb_viewer.import_project,
+          params: {},
+        })
       })
-
-      const dataBaseStore = useDataBaseStore()
-      const treeviewStore = useTreeviewStore()
-      const hybridViewerStore = useHybridViewerStore()
-      treeviewStore.clear()
-      dataBaseStore.clear()
-      hybridViewerStore.clear()
-
-      const schemaImport = back_schemas.opengeodeweb_back.import_project
-      const form = new FormData()
-      form.append("file", file, file?.name)
-
-      const result = await $fetch(schemaImport.$id, {
-        baseURL: geode.base_url,
-        method: "POST",
-        body: form,
+      .then(function () {
+        return viewer_call({
+          schema:
+            viewer_schemas.opengeodeweb_viewer.viewer.reset_visualization,
+          params: {},
+        })
       })
-      const snapshot = result?.snapshot ?? {}
+      .then(function () {
+        treeviewStore.clear()
+        dataBaseStore.clear()
+        hybridViewerStore.clear()
 
-      treeviewStore.isImporting = true
-      await treeviewStore.importStores(snapshot?.treeview)
+        const schemaImport = back_schemas.opengeodeweb_back.import_project
+        const form = new FormData()
+        const fileName = file && file.name ? file.name : "file"
+        form.append("file", file, fileName)
 
-      await hybridViewerStore.initHybridViewer()
-      await hybridViewerStore.importStores(snapshot?.hybridViewer)
+        return $fetch(schemaImport.$id, {
+          baseURL: geode.base_url,
+          method: "POST",
+          body: form,
+        })
+      })
+      .then(function (result) {
+        const snapshot = result && result.snapshot ? result.snapshot : {}
 
-      const snapshotDataBase = snapshot?.dataBase?.db || {}
-      const items = Object.entries(snapshotDataBase).map(([id, item]) => ({
-        id,
-        object_type: item.object_type,
-        geode_object: item.geode_object,
-        native_filename: item.native_filename,
-        viewable_filename: item.viewable_filename,
-        displayed_name: item.displayed_name,
-        vtk_js: { binary_light_viewable: item?.vtk_js?.binary_light_viewable },
-      }))
+        treeviewStore.isImporting = true
 
-      await importWorkflowFromSnapshot(items)
+        return Promise.resolve()
+          .then(function () {
+            return treeviewStore.importStores(snapshot.treeview)
+          })
+          .then(function () {
+            return hybridViewerStore.initHybridViewer()
+          })
+          .then(function () {
+            return hybridViewerStore.importStores(snapshot.hybridViewer)
+          })
+          .then(function () {
+            const snapshotDataBase =
+              snapshot && snapshot.dataBase && snapshot.dataBase.db
+                ? snapshot.dataBase.db
+                : {}
+            const items = Object.entries(snapshotDataBase).map(function (pair) {
+              const id = pair[0]
+              const item = pair[1]
+              const binaryLightViewable =
+                item && item.vtk_js && item.vtk_js.binary_light_viewable
+                  ? item.vtk_js.binary_light_viewable
+                  : undefined
+              return {
+                id: id,
+                object_type: item.object_type,
+                geode_object: item.geode_object,
+                native_filename: item.native_filename,
+                viewable_filename: item.viewable_filename,
+                displayed_name: item.displayed_name,
+                vtk_js: { binary_light_viewable: binaryLightViewable },
+              }
+            })
 
-      await hybridViewerStore.importStores(snapshot?.hybridViewer)
-
-      const dataStyleStore = useDataStyleStore()
-      await dataStyleStore.importStores(snapshot?.dataStyle)
-      await dataStyleStore.applyAllStylesFromState()
-
-      treeviewStore.finalizeImportSelection()
-      treeviewStore.isImporting = false
-    } finally {
-      geode.stop_request()
-    }
+            return importWorkflowFromSnapshot(items)
+          })
+          .then(function () {
+            return hybridViewerStore.importStores(snapshot.hybridViewer)
+          })
+          .then(function () {
+            const dataStyleStore = useDataStyleStore()
+            return dataStyleStore.importStores(snapshot.dataStyle)
+          })
+          .then(function () {
+            const dataStyleStore = useDataStyleStore()
+            return dataStyleStore.applyAllStylesFromState()
+          })
+          .then(function () {
+            treeviewStore.finalizeImportSelection()
+            treeviewStore.isImporting = false
+          })
+      })
+      .finally(function () {
+        geode.stop_request()
+      })
   }
 
   return { exportProject, importProjectFile }
