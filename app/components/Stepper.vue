dc6877c5666336895182b4f22111577edffe189c
SpliiT <spliitdev@gmail.com>
2026-02-23 17:11:36 +0100

    rm useless div

--- app/components/Stepper.vue
+++ app/components/Stepper.vue
@@ -7,38 +7,36 @@
 </script>
 
 <template>
-  <div class="stepper-root fill-height d-flex flex-column">
-    <v-card-item class="flex-shrink-0 pa-0">
-      <div class="px-8 pt-8 pb-4">
-        <div class="d-flex align-center mb-2">
-          <v-icon
-            icon="mdi-file-upload-outline"
-            class="mr-3 title-text"
-            size="32"
-          />
-          <h2 class="title-text">Import Data</h2>
-        </div>
-        <p class="text-white text-body-1">
-          Select and configure your files for a seamless import.
-        </p>
+  <v-card-item class="flex-shrink-0 pa-0">
+    <div class="px-8 pt-8 pb-4">
+      <div class="d-flex align-center mb-2">
+        <v-icon
+          icon="mdi-file-upload-outline"
+          class="mr-3 title-text"
+          size="32"
+        />
+        <h2 class="title-text">Import Data</h2>
       </div>
-    </v-card-item>
+      <p class="text-white text-body-1">
+        Select and configure your files for a seamless import.
+      </p>
+    </div>
+  </v-card-item>
 
-    <v-stepper-vertical
-      :model-value="current_step_index + 1"
-      @update:model-value="current_step_index = $event - 1"
-      flat
-      non-linear
-      class="pa-0 ma-0 bg-transparent rounded-xl overflow-hidden custom-stepper flex-grow-1"
-    >
-      <Step
-        v-for="(step, index) in steps"
-        :key="index"
-        :step_index="index"
-        @reset_values="emit('reset_values')"
-      />
-    </v-stepper-vertical>
-  </div>
+  <v-stepper-vertical
+    :model-value="current_step_index + 1"
+    @update:model-value="current_step_index = $event - 1"
+    flat
+    non-linear
+    class="pa-0 ma-0 bg-transparent rounded-xl overflow-hidden custom-stepper flex-grow-1"
+  >
+    <Step
+      v-for="(step, index) in steps"
+      :key="index"
+      :step_index="index"
+      @reset_values="emit('reset_values')"
+    />
+  </v-stepper-vertical>
 </template>
 
 <style scoped>
