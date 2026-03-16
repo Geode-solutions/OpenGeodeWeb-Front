67c70a7da5890abdab62dd56a2d32504880a7a01
SpliiT <106495600+SpliiT@users.noreply.github.com>
2026-03-16 15:27:00 +0000

    Apply prepare changes

--- internal/database/base_database.js
+++ internal/database/base_database.js
@@ -6,21 +6,21 @@ import { dataStyleTable } from "./tables/data_style"
 import { modelComponentDataStyleTable } from "./tables/model_component_datastyle"
 
 export class BaseDatabase extends Dexie {
-    constructor(name) {
-        super(name)
-    }
+  constructor(name) {
+    super(name)
+  }
 
-    static get initialStores() {
-        return {
-            [dataTable.name]: dataTable.schema,
-            [modelComponentsTable.name]: modelComponentsTable.schema,
-            [dataStyleTable.name]: dataStyleTable.schema,
-            [modelComponentDataStyleTable.name]: modelComponentDataStyleTable.schema,
-            [modelComponentsRelationTable.name]: modelComponentsRelationTable.schema,
-        }
+  static get initialStores() {
+    return {
+      [dataTable.name]: dataTable.schema,
+      [modelComponentsTable.name]: modelComponentsTable.schema,
+      [dataStyleTable.name]: dataStyleTable.schema,
+      [modelComponentDataStyleTable.name]: modelComponentDataStyleTable.schema,
+      [modelComponentsRelationTable.name]: modelComponentsRelationTable.schema,
     }
+  }
 
-    async clear() {
-        return Promise.all(this.tables.map((table) => table.clear()))
-    }
+  async clear() {
+    return Promise.all(this.tables.map((table) => table.clear()))
+  }
 }
