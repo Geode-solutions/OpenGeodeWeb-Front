f53283b0bf7e6a6e07b99768ca2d7b1c005a5793
SpliiT <spliitdev@gmail.com>
2026-03-19 14:22:08 +0100

    use lodash merge

--- internal/stores/data_style/mesh/edges/common.js
+++ internal/stores/data_style/mesh/edges/common.js
@@ -1,25 +1,10 @@
+import merge from "lodash/merge"
 import { useDataStyleStateStore } from "@ogw_internal/stores/data_style/state"
 
 export function useMeshEdgesCommonStyle() {
   const dataStyleStateStore = useDataStyleStateStore()
 
   function mutateMeshEdgesStyle(id, values) {
-    const merge = (target, source) => {
-      for (const [key, value] of Object.entries(source)) {
-        if (
-          value !== null &&
-          typeof value === "object" &&
-          !Array.isArray(value)
-        ) {
-          if (!(key in target)) {
-            target[key] = {}
-          }
-          merge(target[key], value)
-        } else {
-          target[key] = value
-        }
-      }
-    }
     return dataStyleStateStore.mutateStyle(id, (style) => {
       merge(style.edges, values)
     })
