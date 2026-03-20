a8765c9039f0f4ece738599bf231e2101909649f
SpliiT <spliitdev@gmail.com>
2026-03-20 15:55:38 +0100

    rm useless import

--- internal/stores/data_style/mesh/cells/index.js
+++ internal/stores/data_style/mesh/cells/index.js
@@ -1,7 +1,6 @@
 // Third party imports
 
 // Local imports
-import { useDataStyleStateStore } from "@ogw_internal/stores/data_style/state"
 import { useMeshCellsCellAttributeStyle } from "./cell"
 import { useMeshCellsColorStyle } from "./color"
 import { useMeshCellsCommonStyle } from "./common"
