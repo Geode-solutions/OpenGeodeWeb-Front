0b74d783ef10c71da4208f223434d62a7980d6e8
SpliiT <spliitdev@gmail.com>
2026-03-25 15:35:14 +0100

    viewer ui

---
+++ app/components/Viewer/UI.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ViewerContextMenu from "@ogw_front/components/Viewer/ContextMenu";
+import ViewerTreeObjectTree from "@ogw_front/components/Viewer/Tree/ObjectTree";
+
+defineProps({
+  id: { type: String, required: true },
+  displayMenu: { type: Boolean, required: true },
+  menuStore: { type: Object, required: true },
+  containerWidth: { type: Number, required: true },
+  containerHeight: { type: Number, required: true },
+});
+
+const emit = defineEmits(["show-menu"]);
+</script>
+
+<template>
+  <ViewerTreeObjectTree @show-menu="(args) => emit('show-menu', args)" />
+  <ViewerContextMenu
+    v-if="displayMenu"
+    :id="menuStore.current_id || id"
+    :x="menuStore.menuX"
+    :y="menuStore.menuY"
+    :container-width="containerWidth"
+    :container-height="containerHeight"
+  />
+</template>
