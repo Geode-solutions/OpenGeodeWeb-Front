cc8c8c4468b98da9d83d18253c74d1554b4ebc66
MaxNumerique <maximilienlandois@gmail.com>
2026-03-31 16:13:42 +0200

    fix(contextMenu): filter viewer picked IDs by active data items using the data store

--- app/components/Viewer/Ui.vue
+++ app/components/Viewer/Ui.vue
@@ -1,6 +1,6 @@
 <script setup>
 import ViewerContextMenu from "@ogw_front/components/Viewer/ContextMenu";
-import ViewerTreeObjectTree from "@ogw_front/components/Viewer/Tree/ObjectTree";
+import { useDataStore } from "@ogw_front/stores/data";
 import viewer_schemas from "@geode/opengeodeweb-viewer/opengeodeweb_viewer_schemas.json";
 
 const { id, displayMenu, menuStore, containerWidth, containerHeight, dataStyleStore, viewerStore } =
@@ -16,8 +16,14 @@ const { id, displayMenu, menuStore, containerWidth, containerHeight, dataStyleSt
 
 const emit = defineEmits(["show-menu", "set-id"]);
 
+const dataStore = useDataStore();
+const dataItems = dataStore.refAllItems();
+
 async function get_viewer_id(x, y) {
-  const ids = Object.keys(dataStyleStore.styles);
+  const activeIds = new Set(dataItems.value.map((item) => item.id));
+  const ids = Object.keys(dataStyleStore.styles).filter((styleId) =>
+    activeIds.has(styleId),
+  );
   await viewerStore.request(
     viewer_schemas.opengeodeweb_viewer.viewer.picked_ids,
     { x, y, ids },
