1464995af0b7cc59a5ffa9fb9d609b846195e965
JulienChampagnol <91873154+JulienChampagnol@users.noreply.github.com>
2026-04-03 09:56:53 +0000

    Apply prepare changes

--- tests/browser/Viewer/Options/ColorPicke.test.js
+++ tests/browser/Viewer/Options/ColorPicke.test.js
@@ -1,14 +1,14 @@
-import { expect, test } from "vitest"
-import { render } from "vitest-browser-vue"
+import { expect, test } from "vitest";
+import { render } from "vitest-browser-vue";
 
-import ColorPicker from "@/components/Viewer/Options/ColorPicker"
-import { vuetify } from "../../../utils"
+import ColorPicker from "@/components/Viewer/Options/ColorPicker";
+import { vuetify } from "../../../utils";
 
 test("Graphic test for ColorPicker", async () => {
   const component = await render(ColorPicker, {
     global: {
       plugins: [vuetify],
     },
-  })
-  await expect(component.container).toMatchScreenshot()
-})
+  });
+  await expect(component.container).toMatchScreenshot();
+});
