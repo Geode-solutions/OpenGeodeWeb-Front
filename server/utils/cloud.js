93c714cdd9d2bb58a34a6a8e2c5ac3a9fbd064c9
Arnaud Botella <arnaud.botella@geode-solutions.com>
2026-04-10 11:05:09 +0200

    fix(Google): update to new artifact

--- server/utils/cloud.js
+++ server/utils/cloud.js
@@ -13,13 +13,13 @@ function checkRecaptchaParams(name, email, launch) {
 async function artifactImage(registry, parent, repo) {
   const branch = process.env.NETLIFY_BRANCH;
   const [_, projectId] = parent.split("/");
-  const repository = `${parent}/repositories/ghcr/packages/geode-solutions%2F`;
+  const repository = `${parent}/repositories/github/packages/`;
   const name = `${repository}${repo}/tags/${branch}`;
   console.log({ name });
   const response = await registry.projects.locations.repositories.packages.tags.get({
     name,
   });
-  const artifactRegistry = `europe-west9-docker.pkg.dev/${projectId}/ghcr/geode-solutions`;
+  const artifactRegistry = `europe-west9-docker.pkg.dev/${projectId}/github`;
   const digest = response.data.version.split("/").pop();
   const image = `${artifactRegistry}/${repo}@${digest}`;
   console.log("Found image for", repo, image);
