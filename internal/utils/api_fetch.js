24aac48bb30d8f18499891371a7437695a358949
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-04-21 15:30:42 +0200

    cleanup await async

--- internal/utils/api_fetch.js
+++ internal/utils/api_fetch.js
@@ -4,7 +4,7 @@ import { validate_schema } from "@ogw_front/utils/validate_schema";
 
 const ERROR_400 = 400;
 
-export async function api_fetch(
+export function api_fetch(
   microservice,
   { schema, params },
   { request_error_function, response_function, response_error_function, timeout } = {},
@@ -67,7 +67,7 @@ export async function api_fetch(
   }
 
   if (timeout !== undefined && timeout > 0) {
-    return await pTimeout(performCall(), {
+    return pTimeout(performCall(), {
       milliseconds: timeout,
       message: `${schema.$id}: Timed out after ${timeout}ms`,
     });
