5b71fc36e3d37734e40c412b85c1a97c864da8ba
SpliiT <spliitdev@gmail.com>
2026-05-13 14:16:52 +0200

    simplify colormap

--- app/utils/colormap.js
+++ app/utils/colormap.js
@@ -1,14 +1,11 @@
 import scientificPresets from "@ogw_front/assets/scientific_colormaps.json";
-import vtkColorMaps from "@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps";
 
 function getRGBPointsFromPreset(presetName) {
-  for (const category of scientificPresets) {
-    const scientificPreset = category.Children.find((preset) => preset.Name === presetName);
-    if (scientificPreset) {
-      return scientificPreset.RGBPoints;
-    }
-  }
-  return vtkColorMaps.getPresetByName(presetName)?.RGBPoints ?? [];
+  return (
+    scientificPresets
+      .flatMap((category) => category.Children)
+      .find((preset) => preset.Name === presetName)?.RGBPoints ?? []
+  );
 }
 
 export { getRGBPointsFromPreset, scientificPresets };
