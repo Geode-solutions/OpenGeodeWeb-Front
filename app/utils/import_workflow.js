00e7aa631d1972a8a41f994e5bd6e2adb0f4edd3
SpliiT <spliitdev@gmail.com>
2026-05-22 15:09:03 +0200

    fix(EmptyPointSet): Add feedback and fix pointset empty

--- app/utils/import_workflow.js
+++ app/utils/import_workflow.js
@@ -6,6 +6,7 @@ import back_schemas from "@geode/opengeodeweb-back/opengeodeweb_back_schemas.jso
 import { useBackStore } from "@ogw_front/stores/back";
 import { useDataStore } from "@ogw_front/stores/data";
 import { useDataStyleStore } from "@ogw_front/stores/data_style";
+import { useFeedbackStore } from "@ogw_front/stores/feedback";
 import { useHybridViewerStore } from "@ogw_front/stores/hybrid_viewer";
 import { useTreeviewStore } from "@ogw_front/stores/treeview";
 
@@ -31,6 +32,11 @@ async function importItem(item) {
   const hybridViewerStore = useHybridViewerStore();
   const treeviewStore = useTreeviewStore();
 
+  if (item.nb_vertices === 0) {
+    const feedbackStore = useFeedbackStore();
+    feedbackStore.add_warning(`Pointset "${item.name}" is empty`);
+  }
+
   const registerTask = dataStore.registerObject(item.id);
   const addDataTask = dataStore.addItem(item);
   const addDataComponentsTask =
@@ -39,7 +45,12 @@ async function importItem(item) {
     item.viewer_type === "model" ? dataStore.addComponentRelations(item) : Promise.resolve();
   treeviewStore.addItem(item.geode_object_type, item.name, item.id, item.viewer_type);
   const addDataStyleTask = dataStyleStore.addDataStyle(item.id, item.geode_object_type);
-  const addViewerTask = addDataTask.then(() => hybridViewerStore.addItem(item.id));
+  const addViewerTask = addDataTask.then(() => {
+    if (item.nb_vertices === 0) {
+      return;
+    }
+    return hybridViewerStore.addItem(item.id);
+  });
   const applyStyleTask = Promise.all([registerTask, addDataComponentsTask, addDataStyleTask]).then(
     () => dataStyleStore.applyDefaultStyle(item.id),
   );
