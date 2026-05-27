90997700966d30f24b6f95c2c29b573199508b2d
SpliiT <106495600+SpliiT@users.noreply.github.com>
2026-05-27 15:49:38 +0000

    Apply prepare changes

--- app/components/Viewer/Options/ColormapQuickPicker.vue
+++ app/components/Viewer/Options/ColormapQuickPicker.vue
@@ -37,8 +37,7 @@ const current = computed(() => {
   for (const { key, getterKey } of componentNames) {
     const activeColoring = style[key]?.coloring?.active;
     if (["vertex", "edge", "polygon", "cell", "polyhedron"].includes(activeColoring)) {
-      const attributeType =
-        `${activeColoring.charAt(0).toUpperCase()}${activeColoring.slice(1)}Attribute`;
+      const attributeType = `${activeColoring.charAt(0).toUpperCase()}${activeColoring.slice(1)}Attribute`;
       const getterName = `${getterKey}${attributeType}ColorMap`;
       const getter = dataStyleStore[getterName];
       if (getter) {
@@ -80,8 +79,7 @@ async function onQuickColormapSelect(preset) {
   for (const { key, setterKey } of componentNames) {
     const activeColoring = style[key]?.coloring?.active;
     if (["vertex", "edge", "polygon", "cell", "polyhedron"].includes(activeColoring)) {
-      const attributeType =
-        `${activeColoring.charAt(0).toUpperCase() + activeColoring.slice(1)  }Attribute`;
+      const attributeType = `${activeColoring.charAt(0).toUpperCase() + activeColoring.slice(1)}Attribute`;
       const setterName = `set${setterKey}${attributeType}ColorMap`;
       const setter = dataStyleStore[setterName];
       if (setter) {
