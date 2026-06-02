08b5d805f9746c63d524653ab0fdb3d0be099407
JulienChampagnol <91873154+JulienChampagnol@users.noreply.github.com>
2026-06-02 15:21:04 +0000

    Apply prepare changes

--- server/api/extensions/run.post.js
+++ server/api/extensions/run.post.js
@@ -10,7 +10,10 @@ import {
   addMicroserviceMetadatas,
   runBack,
 } from "@geode/opengeodeweb-front/app/utils/local/microservices.js";
-import { executableName, extensionFrontendPath } from "@geode/opengeodeweb-front/app/utils/local/path.js";
+import {
+  executableName,
+  extensionFrontendPath,
+} from "@geode/opengeodeweb-front/app/utils/local/path.js";
 import { extensionsConf } from "@geode/opengeodeweb-front/app/utils/config.js";
 import { unzipFile } from "@geode/opengeodeweb-front/app/utils/server.js";
 
@@ -67,7 +70,10 @@ export default defineEventHandler(async (event) => {
         console.log("runExtensions", { frontendFilePath });
         const frontendContent = await fs.promises.readFile(frontendFilePath, "utf8");
 
-        const backendExecutablePath = path.join(unzippedExtensionPath, executableName(backendExecutable));
+        const backendExecutablePath = path.join(
+          unzippedExtensionPath,
+          executableName(backendExecutable),
+        );
         console.log("runExtensions", { backendExecutablePath });
         fs.chmodSync(backendExecutablePath, "755");
         const port = await runBack(backendExecutable, unzippedExtensionPath, {
