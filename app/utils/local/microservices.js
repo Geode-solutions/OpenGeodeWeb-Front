3824442f34215bde2115d43ed876a5475311ef86
Tao Schiro <spliitdev@gmail.com>
2026-07-13 16:04:15 +0200

    try with more timeout value

--- app/utils/local/microservices.js
+++ app/utils/local/microservices.js
@@ -12,7 +12,7 @@ import { microservicesMetadatasPath, projectMicroservices } from "./cleanup.js";
 import { executablePath } from "./path.js";
 
 const MILLISECONDS_PER_SECOND = 1000;
-const DEFAULT_TIMEOUT_SECONDS = 30;
+const DEFAULT_TIMEOUT_SECONDS = 120;
 
 async function runScript(
   execPath,
