12f6de21faccc5c05020425b313a8b370d11be0d
Tao Schiro <spliitdev@gmail.com>
2026-07-24 10:30:09 +0200

    clean undefined

--- app/utils/import_workflow.js
+++ app/utils/import_workflow.js
@@ -62,14 +62,14 @@ async function importItem(item) {
   treeviewStore.addItem(item.geode_object_type, item.name, item.id, item.viewer_type);
   const addDataStyleTask = dataStyleStore.addDataStyle(item.id, item.geode_object_type);
   const addViewerTask = addDataTask.then(() => {
-    if (item.nb_vertices === 0 || item.viewable_file === "") {
+    if (item.nb_vertices === 0 || !item.viewable_file) {
       return;
     }
     return hybridViewerStore.addItem(item.id);
   });
   const applyStyleTask = Promise.all([registerTask, addDataComponentsTask, addDataStyleTask]).then(
     () => {
-      if (item.viewable_file !== "") {
+      if (item.viewable_file) {
         return dataStyleStore.applyDefaultStyle(item.id);
       }
       return undefined;
