603f11d8d49045603061921050577c507c0f91c4
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-08-03 14:16:27 +0200

    harmonine functions

---
+++ server/api/microservice/app/set_viewer_base_url.post.js
@@ -0,0 +1,22 @@
+// Third party imports
+import { createError, defineEventHandler, readBody } from "h3";
+
+// Local imports
+import { setViewerBaseUrl } from "@vease_server/utils/config.js";
+
+export default defineEventHandler(async (event) => {
+  try {
+    const { baseUrl } = await readBody(event);
+    if (!baseUrl) {
+      throw createError({ statusCode: 400, statusMessage: "baseUrl is required" });
+    }
+    await setViewerBaseUrl(baseUrl);
+    return { statusCode: 200, baseUrl };
+  } catch (error) {
+    console.log(error);
+    throw createError({
+      statusCode: error.statusCode,
+      statusMessage: error.message,
+    });
+  }
+});
