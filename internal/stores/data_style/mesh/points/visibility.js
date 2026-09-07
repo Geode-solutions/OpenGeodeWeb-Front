165a35572d0153b13a50b64ec38f83876bf167c5
MaxNumerique <maximilienlandois@gmail.com>
2026-09-07 14:31:36 +0200

    revert

--- internal/stores/data_style/mesh/points/visibility.js
+++ internal/stores/data_style/mesh/points/visibility.js
@@ -23,7 +23,9 @@ export function useMeshPointsVisibilityStyle() {
         params,
       },
       {
-        response_function: () => meshPointsCommonStyle.mutateMeshPointsStyle(id, { visibility }),
+        response_function(response) {
+          return meshPointsCommonStyle.mutateMeshPointsVisibility(response);
+        },
       },
     );
   }
