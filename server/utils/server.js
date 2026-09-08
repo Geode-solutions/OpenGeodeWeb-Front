f7b07e35958e7cbf7abb0d9a5f7f97642f602adb
JulienChampagnol <julien.champagnol@geode-solutions.com>
2026-09-08 11:05:15 +0200

    handle tar.gz archives

--- server/utils/server.js
+++ server/utils/server.js
@@ -4,34 +4,51 @@ import path from "node:path";
 
 // Third party imports
 import JSZip from "jszip";
+import { extract as extractTar } from "tar";
+
+const TAR_ARCHIVE_PATTERN = /\.(tar\.gz|tgz|tar)$/u;
+
+async function extractZipArchive(zipFilePath, outputDir) {
+  const data = await fs.promises.readFile(zipFilePath);
+  const zip = await JSZip.loadAsync(data);
+  const promises = [];
+
+  for (const [relativePath, zipEntry] of Object.entries(zip.files)) {
+    const outputPath = path.join(outputDir, relativePath);
+
+    if (zipEntry.dir) {
+      promises.push(fs.promises.mkdir(outputPath, { recursive: true }));
+    } else {
+      promises.push(
+        (async () => {
+          const content = await zipEntry.async("nodebuffer");
+          await fs.promises.mkdir(path.dirname(outputPath), {
+            recursive: true,
+          });
+          await fs.promises.writeFile(outputPath, content);
+        })(),
+      );
+    }
+  }
+
+  await Promise.all(promises);
+}
+
+async function extractTarArchive(archivePath, outputDir) {
+  await extractTar({ file: archivePath, cwd: outputDir });
+}
 
 async function unzipFile(zipFilePath, outputDir = zipFilePath.replace(/\.[^/.]+$/u, "")) {
   console.log("Unzipping file...", zipFilePath, outputDir);
   try {
-    const data = await fs.promises.readFile(zipFilePath);
-    const zip = await JSZip.loadAsync(data);
     await fs.promises.mkdir(outputDir, { recursive: true });
-    const promises = [];
 
-    for (const [relativePath, zipEntry] of Object.entries(zip.files)) {
-      const outputPath = path.join(outputDir, relativePath);
-
-      if (zipEntry.dir) {
-        promises.push(fs.promises.mkdir(outputPath, { recursive: true }));
-      } else {
-        promises.push(
-          (async () => {
-            const content = await zipEntry.async("nodebuffer");
-            await fs.promises.mkdir(path.dirname(outputPath), {
-              recursive: true,
-            });
-            await fs.promises.writeFile(outputPath, content);
-          })(),
-        );
-      }
+    if (TAR_ARCHIVE_PATTERN.test(zipFilePath)) {
+      await extractTarArchive(zipFilePath, outputDir);
+    } else {
+      await extractZipArchive(zipFilePath, outputDir);
     }
 
-    await Promise.all(promises);
     console.log("Extraction complete!");
     return outputDir;
   } catch (error) {
